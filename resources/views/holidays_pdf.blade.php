da9201c81380a7269603377e093f7333934172f6
zumbani <info@mydeveloper.co.za>
2022-04-20 18:17:18 +0200

    Fix blank pages on PDF file

--- resources/views/holidays_pdf.blade.php
+++ resources/views/holidays_pdf.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 <body>    
-<div class="container mt-5">
+<div class="container">
     <h2 class="mb-4">South African Holidays</h2>
     <table class="table table-bordered holiday-datatable">
         <thead>
@@ -30,14 +30,4 @@
     </table>
 </div>   
 </body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
-<script type="text/javascript">
-  $(function () {    
-    var table = $('.holiday-datatable').DataTable({ });    
-  });
-</script>
 </html>
