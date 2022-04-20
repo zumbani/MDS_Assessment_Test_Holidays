dc58b384024f9fb86e915adf94a1327e5289c755
zumbani <info@mydeveloper.co.za>
2022-04-20 17:49:13 +0200

    Change the Download Pdf text

--- resources/views/holidays.blade.php
+++ resources/views/holidays.blade.php
@@ -10,7 +10,7 @@
 <body>    
 <div class="container mt-5">
     <h2 class="mb-4">South African Holidays</h2>
-    <a href="{{ route('holidays.pdf') }}">Holiday PDF </a>
+    <a href="{{ route('holidays.pdf') }}">Download Pdf</a>
     <table class="table table-bordered holiday-datatable">
         <thead>
             <tr>
